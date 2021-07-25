c63981537705830c2e07f7ede1348bb45fcdf05c
LeandroSannt <lsn_slim@yahoo.com.br>
2021-07-25 14:53:35 -0300

    criando as tabelas

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,25 @@
+import {Entity, Column, PrimaryGeneratedColumn,CreateDateColumn,UpdateDateColumn,OneToMany,JoinColumn} from 'typeorm'
+import { v4 as uuid } from "uuid";
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
 export default Category;
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,31 @@
+import {Entity, Column, PrimaryGeneratedColumn,CreateDateColumn,UpdateDateColumn,OneToMany,JoinColumn} from 'typeorm'
+import { v4 as uuid } from "uuid";
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
-  category_id: string;
-
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
 export default Transaction;

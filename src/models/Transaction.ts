ef328e7343ffa47fc9eeceb4b2b9aff72e1afbf8
LeandroSannt <lsn_slim@yahoo.com.br>
2021-07-26 23:47:50 -0300

    criando transações e categorias

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,5 +1,7 @@
-import {Entity, Column, PrimaryGeneratedColumn,CreateDateColumn,UpdateDateColumn,OneToMany,JoinColumn} from 'typeorm'
+import {Entity, Column, PrimaryGeneratedColumn,CreateDateColumn,UpdateDateColumn,ManyToOne,JoinColumn} from 'typeorm'
 import { v4 as uuid } from "uuid";
+
+import Category from './Category'
 
 @Entity('transactions')
 class Transaction {
@@ -14,6 +16,13 @@ class Transaction {
 
   @Column()
   value: number;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({name:'category_id'})
+  category:Category
+
+  @Column()
+  category_id:string
 
   @CreateDateColumn()
   created_at: Date;
